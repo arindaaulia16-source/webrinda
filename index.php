2c75748e1b840123519431cd7d740dba6320ee6e
arindaaulia16-source <arindaaulia16@gmail.com>
2025-10-20 13:53:11 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Layout Mockup Dengan Semantik HTML</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="wrapper">
+  <div id="container">
+
+    <header><img src="assets/images/header.jpg" alt="header"></header>
+
+    <nav>
+      <ul>
+        <li><a href="index.php">Beranda</a></li>
+        <li><a href="#">Artikel</a></li>
+        <li class="dropdown">
+          <a href="#">Profil</a>
+          <ul class="submenu">
+            <li><a href="#">Hub Kami</a></li>
+            <li><a href="#">Tentang Kami</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+
+    <article>
+      <div id="single">
+        <div id="t-images" style="background:url(assets/images/content1.jpg)">&nbsp;</div>
+        <h2>Belajar Web dengan HTML+CSS3</h2>
+        <p>
+          Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document. To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries. Themes and styles also help keep your document coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt graphics change to match your new theme. When you apply styles, your headings change to match the new theme. Save time in Word with new buttons that show up where you need them.To change the way a picture fits in your document, click it and a button for layout options appears next to it. When you work on a table, click where you want to add a row or a column, and then click the plus sign. Reading is easier, too, in the new Reading view. You can collapse parts of the document and focus on the text you want. 
+
+        </p>
+        <div id="more"><a href="content1.php">Baca selengkapnya</a></div>
+      </div>
+
+      <div id="single">
+        <div id="t-images" style="background:url(assets/images/content2.jpg)">&nbsp;</div>
+        <h2>Mockup web dengan word</h2>
+        <p>
+          Click Insert and then choose the elements you want from the different galleries. Themes and styles also help keep your document coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt graphics change to match your new theme. When you apply styles, your headings change to match the new theme. Save time in Word with new buttons that show up where you need them. To change the way a picture fits in your document, click it and a button for layout options appears next to it. When you work on a table, click where you want to add a row or a column, and then click the plus sign. Reading is easier, too, in the new Reading view. You can collapse parts of the document and focus on the text you want. If you need to stop reading before you reach the end, Word remembers where you left off - even on another device. Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document. To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. 
+
+        </p>
+        <div id="more"><a href="content2.php">Baca selengkapnya</a></div>
+      </div>
+    </article>
+
+    <aside>
+      <li class="widget">
+        <h2>Berita Populer</h2>
+        <ul>													
+          <li><a href="#">Belajar Web dengan HTML+CSS3</a></li> 
+          <li><a href="#">Mockup web dengan Word</a></li>
+          <li><a href="#">Disain Web Statis</a></li>
+        </ul>
+      </li>
+
+      <li class="widget">
+        <h2>Artikel Populer</h2>
+        <ul>
+          <li><a href="#">Artikel 1</a></li>
+          <li><a href="#">Artikel 2</a></li>
+          <li><a href="#">Artikel 3</a></li>
+        </ul>
+      </li>
+
+      <li class="widget">
+        <h2>Berita selanjutnya</h2>
+        <ul>
+          <li><a href="#">UINSU Ganti Logo</a></li>
+          <li><a href="#">RUSIA Damai</a></li>
+          <li><a href="#">Cuaca di Medan</a></li>
+        </ul>
+      </li>
+    </aside>
+
+    <footer>
+      <div id="column">
+        <h2>LAYANAN</h2>
+        E-Mail<br>
+        E-Learning,BR.<br>
+        E-Jurnal<br>
+        Digital Library<br>
+        Portal SIA<br>
+        SI-Selma<br>
+        SI-Dahlia
+      </div>
+
+      <div id="column">
+        <h2>Business Hour</h2>
+        Silahkan hubungi pesbangnis UINSU telp 064123123 pada
+        jam kerja atau email <a href="#">pusbangnis@uinsu.ac.id</a>
+      </div>
+
+      <div id="column">
+        <h2>Head Office</h2>
+        Jl. Willem Iskandar, Pasar V, Medan Estate<br>
+        Tel : +62-61-6615683<br>
+        Email : humas@uinsu.ac.id
+      </div>
+    </footer>
+
+  </div>
+</div>
+
+<script>
+</script>
+</body>
+</html>
